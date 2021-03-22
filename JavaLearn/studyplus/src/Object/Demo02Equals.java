ae1c98a5bc03f73d68e10860fcae7a7e31f5efad
zhangchenyu <zhangcheneyu1023@126.com>
2021-03-22 21:18:27 +0800

    3月22日

---
+++ JavaLearn/studyplus/src/Object/Demo02Equals.java
@@ -0,0 +1,13 @@
+package Object;
+
+public class Demo02Equals {
+    public static void main(String[] args) {
+        Person p1 = new Person("迪丽热巴",18);
+        Person p2 = new Person("古力娜扎",19);
+
+        System.out.println("p1:"+p1);//p1:com.itheima.demo01.Object.Person@58ceff1
+        System.out.println("p2:"+p2);//p2:com.itheima.demo01.Object.Person@7c30a502
+
+        System.out.println(p1.equals(p2));//false
+    }
+}
---
+++ JavaLearn/studyplus/src/ceshi/Hello.java
@@ -0,0 +1,11 @@
+package ceshi;
+
+/**
+ * @author zhangchenyu
+ * @date 2021-03-22 21:02
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
